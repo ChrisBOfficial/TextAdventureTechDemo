a757a92b356819a8723fb14d20bfd8ba2d444b00
Chris Burbach <officialchrisb@icloud.com>
2017-01-19 14:47:43 -0600

    Minor combat fixes
    Fixed combat systems, minor text fixes, and stat adjustments

---
+++ Monster.java
@@ -0,0 +1,75 @@
+import java.io.IOException;
+import java.lang.*;
+import java.util.Scanner;
+
+public class Monster {
+
+    private String name;
+    int level;
+    private String affixName;
+    double HP;
+    double damage;
+    int attack;
+    int armorClass;
+    int makeAttack;
+    double speed;
+    double xpVal;
+    /*
+    0-80 Normal
+    81-90 Bolstered, more HP
+    91-95 Mighty, more HP and damage
+    96-100 Tyrannical, much more HP and damage
+     */
+
+    String getName() {
+        return name;
+    }
+
+    void setMakeAttack () {
+        makeAttack = (int)(Math.random() * 20 + 1) + attack;
+    }
+
+    void setHealth(double x) {
+        HP -= x;
+    }
+
+    void setXpVal (double x) {
+        xpVal = x;
+    }
+
+    Monster (String n, int l, int d, double s, int AC, int affix) {
+        name = n;
+        level = l;
+        HP = (level * 7);
+        damage = d;
+        speed = level * s;
+        attack = level;
+        armorClass = AC;
+        xpVal = (HP / 100) * 2;
+        if(affix > 80 && affix < 91) {
+            affixName = "Bolstered ";
+            name = affixName + n;
+            HP *= 1.4;
+            armorClass += 3;
+            xpVal *= 1.5;
+        } else if(affix > 90 && affix < 96) {
+            affixName = "Mighty ";
+            name = affixName + n;
+            HP *= 1.5;
+            damage *= 1.2;
+            armorClass += 5;
+            attack += 2;
+            xpVal *= 1.8;
+        } else if(affix > 95 && affix < 101) {
+            affixName = "Tyrannical ";
+            name = affixName + n;
+            HP *= 1.5;
+            damage *= 1.4;
+            attack += 2;
+            xpVal *= 2.5;
+        }
+    }
+
+    public static void main (String str[]) {
+    }
+}
---
+++ TextAdventure.java
@@ -0,0 +1,168 @@
+import java.io.IOException;
+import java.lang.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class TextAdventure {
+    private static int runWorked = 0;
+
+    private static void timerDelay(int x) {
+        try {
+            Thread.sleep(x);
+        } catch (InterruptedException ex) {
+            System.out.print("Interrupted");
+        }
+    }
+
+    private static void attack(Monster Monster) {
+        System.out.println("You attack...\n");
+        Player.setMakeAttack();
+        Monster.setMakeAttack();
+        if(Monster.HP <= 0) {
+            return;
+        }
+        if(Player.HP <= 0) {
+            return;
+        }
+        timerDelay(500);
+        if(Player.makeAttack >= Monster.armorClass) {
+            Monster.setHealth(Player.damage);
+            System.out.println("Your attack hits! " + Player.damage + " Damage.");
+            healthStats(Monster);
+            if(Monster.HP <= 0)
+                return;
+        } else {
+            System.out.println("Your attack is blocked.");
+        }
+        timerDelay(750);
+        System.out.println("\n" + Monster.getName() + " attacks...\n");
+        timerDelay(500);
+        if(Monster.makeAttack > Player.armorClass) {
+            System.out.println("You're hit! " + Monster.damage + " Damage.");
+            Player.setHealth(-Monster.damage);
+            healthStats(Monster);
+        } else {
+            System.out.println("You block the " + Monster.getName() + "'s attack.");
+        }
+    }
+
+    private static void healthStats(Monster Monster) {
+        System.out.println("Enemy Health: " + Monster.HP +
+                "\nYour Health: " + Player.HP);
+    }
+
+    private static void item() {
+    }
+
+    private static void run(double s, double monS) {
+        runWorked = 0;
+        System.out.println("You attempt to run...");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            System.out.print("Interrupted");
+        }
+        if(s > monS) {
+            System.out.println("\nYou escaped!");
+            runWorked = 1;
+        }
+        else
+            System.out.println("\nYou fail to escape.\n");
+    }
+
+    private static void combatOptions() {
+        System.out.println("---------------------------------" +
+                "\n| 1. ATTACK | 2. ITEM(fix me) | 3. RUN | " +
+                "\n---------------------------------\n");
+    }
+
+    private static void combatStart(Monster Monster) {
+        Scanner scan = new Scanner(System.in);
+
+        while (Player.HP > 0) {
+            timerDelay(250);
+            if(Monster.HP <= 0 || runWorked == 1)
+                break;
+            combatOptions();
+            int choice = scan.nextInt();
+            combatChoice(choice, Monster.speed, Monster);
+        }
+    }
+
+    private static void combatChoice(int x, double y, Monster Monster) {
+        switch (x) {
+            case 1:
+                attack(Monster);
+                break;
+            case 2:
+                item();
+                break;
+            case 3:
+                run(Player.speed, y);
+                break;
+        }
+    }
+
+    private static void combatResult(Monster Monster, double exp) {
+        if(Player.HP > 0 && Monster.HP <= 0) {
+            System.out.println("\nYou win! Current level - " + (int)Player.levelRaw);
+            Player.HP = Player.maxHP;
+        } else if (Monster.HP > 0 && Player.HP <= 0) {
+            Player.setLevel(-exp);
+            System.out.println("\nYou lost... Current level - " + (int)Player.levelRaw);
+        } else
+            System.out.println("Error.");
+        levelUpCheck(Monster.xpVal);
+    }
+
+    private static void levelUpCheck(double exp) {
+        Player.oldLevel = (int)Player.levelRaw;
+        Player.setLevel(exp);
+        if((int)Player.levelRaw - Player.oldLevel >= 3) {
+            Player.oldLevel = (int)Player.levelRaw;
+            System.out.println("Level up! Your stats have increased.");
+            Player.classChoice(Player.archetype);
+            Player.HP = Player.maxHP;
+            timerDelay(500);
+            System.out.println("New level - " + (int)Player.levelRaw);
+        }
+    }
+
+    public static void main (String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int affixMake = (int)(Math.random() * 100);
+
+        System.out.println("Welcome to the TextAdventure TechDemo. What is your name?");
+        String playerName = scan.nextLine();
+
+        System.out.println("Welcome " + playerName + ", which archetype would you like?\n");
+        System.out.println("| 1. WARRIOR | 2. THIEF | 3. KNIGHT |\n");
+        int chosenClass = scan.nextInt();
+        Player.setArch(chosenClass);
+        Player.classChoice(chosenClass);
+
+        System.out.println("Welcome " + Player.classType + ". Now we need to acquaint you with combat.\n\n" +
+                           "| 1. Uh oh. | 2. I'm ready! | 3. No thank you... |\n");
+        Monster tutorialCreature = new Monster("Skeleton",1,1,1.1, 10, affixMake);
+
+        scan.nextInt();
+        System.out.println("Great! You see a level " + tutorialCreature.level + " " +
+                           tutorialCreature.getName() + "! What do you wish to do?\n");
+
+        combatStart(tutorialCreature);
+
+        timerDelay(1000);
+        if(runWorked == 0)
+            combatResult(tutorialCreature, tutorialCreature.xpVal);
+        timerDelay(2000);
+
+        Monster zombie = new Monster("Zombie", 1, 1,0.8, 15, affixMake);
+
+        System.out.println("You are ambushed by a level " + zombie.level + " " + zombie.getName() + "! What do you wish to do?\n");
+
+        combatStart(zombie);
+        timerDelay(1000);
+        if(runWorked == 0)
+            combatResult(zombie, zombie.xpVal);
+    }
+}
